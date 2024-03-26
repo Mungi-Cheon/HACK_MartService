dade921ab9768ae9670e8d01a5e376d442bed5cd
moon-choi <choimy1991@gmail.com>
2024-03-26 19:50:51 +0900

    REFACTOR: Cart.add() if-else문 getOrDefault로 변경

--- src/main/java/thing/Cart.java
+++ src/main/java/thing/Cart.java
@@ -10,24 +10,19 @@ public class Cart {
     private int count = 0;
 
     public void add(Product product, int quantity) {
-        //최대 수량 넘는지 체크
+
         int max = 10;
         if (count + quantity > max) {
             System.out.printf("🚫 상품은 최대 %d개까지 넣을 수 있습니다.\n", max);
             return;
         }
 
-        //카트에 존재하는 지 체크
         String name = product.getName();
         Product current;
-        if (map.containsKey(name)) {
-            current = map.get(name); //카트에 이미 있는 객체
-        } else {
-            current = product; //새로운 객체
-        }
-        current.setQuantity(quantity); //현재 넣는 양
-        count += quantity; //카트에 누적되는 양
-        map.put(name, current); //객체 업데이트
+        current = map.getOrDefault(name, product);
+        current.setQuantity(quantity);
+        count += quantity;
+        map.put(name, current);
 
         System.out.printf("✚ %s (%d원) %d개가 카트에 추가되었습니다. (현재까지 %d / %d 개)\n", product.getName(),
             product.getPrice(), quantity, count, max);
